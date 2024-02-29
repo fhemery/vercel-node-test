099a037e00bff2312f71dea3d8d383400d95f307
fhemery <f.hemery@hemit.fr>
2024-02-29 14:00:29 +0100

    Test express-validator

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 // Add Express
 const express = require("express");
+const { query, validationResult  } = require('express-validator');
 
 // Initialize Express
 const app = express();
+app.use(express.json());
 
 // Create GET request
 app.get("/api/ping", (req, res) => {
   res.send("PONG");
+});
+
+app.get('/api/hello', query('person').notEmpty(), (req, res) => {
+  const result = validationResult(req);
+  if (result.isEmpty()) {
+    return res.send(`Hello, ${req.query.person}!`);
+  }
+
+  res.send({ errors: result.array() });
 });
 
 // Initialize server
